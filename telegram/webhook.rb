e65a5d6419d62812fdea26e99df464703e2930a6
Stanislav Shandyga <stanislavshandyga@gmail.com>
2026-05-07 13:28:02 +0000

    Minor code updates

--- telegram/webhook.rb
+++ telegram/webhook.rb
@@ -34,20 +34,20 @@ module Telegram
                 if timezone
                     @telegram_api.send_message(chat_id, "Your timezone is set to #{timezone}")
                 else
-                    @telegram_api.send_message(chat_id, "You haven't set a timezone yet. Use /set_timezone <your_timezone> to set it.")
+                    @telegram_api.send_message(chat_id, "You haven't set a timezone yet. Use /set_timezone to set it.")
                 end
             when '/set_timezone'
                 settings = @upstash.get_hash(chat_id.to_s)
                 settings["#{username}_setting"] = 'awaiting_timezone'
                 @upstash.set_hash(chat_id.to_s, settings)
-                
+
                 @telegram_api.send_message(chat_id, "Enter your timezone (e.g., 'London'):")
             when '/remove_timezone'
                 settings = @upstash.get_hash(chat_id.to_s)
                 settings.delete(username)
                 @upstash.set_hash(chat_id.to_s, settings)
 
-                @telegram_api.send_message(chat_id, "Your timezone has been removed. You can set it again with /set_timezone <your_timezone>")
+                @telegram_api.send_message(chat_id, "Your timezone has been removed. You can set it again with /set_timezone")
             else
                 settings = @upstash.get_hash(chat_id.to_s)
 
