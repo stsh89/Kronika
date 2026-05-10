2e993af437b406f1536cb8489d6be9a82b55aeeb
Stanislav Shandyga <stanislavshandyga@gmail.com>
2026-05-10 12:08:10 +0300

    Fix messaging for time zone identifier errors

--- lib/set_timezone_operation.rb
+++ lib/set_timezone_operation.rb
@@ -6,17 +6,23 @@ class SetTimezoneOperation
         @notification_service = services[:notification]
     end
 
-    def execute(tz_identifier)
+    def execute(tz_identifier)       
         chat = @storage_service.get_chat(@chat_id)
+
+        if tz_identifier == ''
+            @notification_service.send_message(chat, "Missing time zone identifier. Please provide a valid time zone.")
+            
+            return
+        end
         
         begin
             timezone = Timezone.new(tz_identifier)
             chat.users[@username] = User.new(username: @username, timezone: timezone)
 
             @storage_service.save_chat(chat)
-            @notification_service.send_message(chat, "Your timezone has been set to #{timezone.identifier}.")
+            @notification_service.send_message(chat, "Your time zone has been set to #{timezone.identifier}.")
         rescue InvalidArgumentError => e
-            @notification_service.send_message(chat, "Invalid timezone identifier: #{tz_identifier}. Please provide a valid timezone.")
+            @notification_service.send_message(chat, "Invalid time zone identifier: #{tz_identifier}. Please provide a valid time zone.")
         end
     end
 end
