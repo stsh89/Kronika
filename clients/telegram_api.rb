3d834fedaea1739f9a306b92ba2058c764a679af
Stanislav Shandyga <stanislavshandyga@gmail.com>
2026-05-11 06:16:28 +0000

    Lint clients

--- clients/telegram_api.rb
+++ clients/telegram_api.rb
@@ -1,33 +1,35 @@
+# frozen_string_literal: true
+
 require 'async'
 require 'async/http/internet'
 
 class TelegramAPI
-    def initialize
-        token = ENV['TELEGRAM_BOT_TOKEN']
-        @base_url = "https://api.telegram.org/bot#{token}/"
+  def initialize
+    token = ENV.fetch('TELEGRAM_BOT_TOKEN', nil)
+    @base_url = "https://api.telegram.org/bot#{token}/"
+  end
+
+  def send_message(chat_id, text)
+    send_message_with_options(chat_id, text)
+  end
+
+  def send_html_message(chat_id, html)
+    send_message_with_options(chat_id, html, { parse_mode: 'HTML' })
+  end
+
+  private
+
+  def send_message_with_options(chat_id, text, options = {})
+    url = "#{@base_url}sendMessage"
+    body = { chat_id: chat_id, text: text }.merge(options)
+    internet = Async::HTTP::Internet.new
+
+    begin
+      Async::Task.current.with_timeout(3) do
+        internet.post(url, { 'Content-Type': 'application/json' }, [body.to_json])
+      end
+    ensure
+      internet.close
     end
-
-    def send_message(chat_id, text)
-       send_message_with_options(chat_id, text)
-    end
-
-    def send_html_message(chat_id, html)
-        send_message_with_options(chat_id, html, { parse_mode: 'HTML' })
-    end
-
-    private
-
-    def send_message_with_options(chat_id, text, options = {})
-        url = @base_url + "sendMessage"
-        body = { chat_id: chat_id, text: text }.merge(options)
-        internet = Async::HTTP::Internet.new
-
-        begin
-            Async::Task.current.with_timeout(3) do
-                internet.post(url, { 'Content-Type': 'application/json' }, [body.to_json])
-            end
-        ensure
-            internet.close
-        end
-    end
+  end
 end
--- clients/upstash.rb
+++ clients/upstash.rb
@@ -1,59 +1,59 @@
+# frozen_string_literal: true
+
 require 'async'
 require 'async/http/internet'
 
 class Upstash
-    REQUEST_TIMEOUT_IN_SECONDS = 3
+  REQUEST_TIMEOUT_IN_SECONDS = 3
 
-    def initialize
-        @url = ENV['UPSTASH_URL']
-        @token = ENV['UPSTASH_TOKEN']
-        @headers = { 'authorization': "Bearer #{@token}" }
+  def initialize
+    @url = ENV.fetch('UPSTASH_URL', nil)
+    @token = ENV.fetch('UPSTASH_TOKEN', nil)
+    @headers = { authorization: "Bearer #{@token}" }
+  end
+
+  def get_chat_timezones(chat_id)
+    get_hash("chat:#{chat_id}:timezones")
+  end
+
+  def save_chat_timezones(chat_id, timezones)
+    set_hash("chat:#{chat_id}:timezones", timezones)
+  end
+
+  private
+
+  def get_hash(key)
+    url = "#{@url}/get/#{key}"
+    internet = Async::HTTP::Internet.new
+
+    begin
+      response = Async::Task.current.with_timeout(REQUEST_TIMEOUT_IN_SECONDS) do
+        internet.get(url, @headers)
+      end
+
+      raise "Upstash API error. Status: #{response.status}. Body: #{response.read}" unless response.success?
+
+      body = response.read
+      result = JSON.parse(body)['result']
+
+      return {} if result.nil?
+
+      JSON.parse(result).to_h
+    ensure
+      internet.close
     end
+  end
 
-    def get_chat_timezones(chat_id)
-        get_hash("chat:#{chat_id}:timezones")
+  def set_hash(key, value)
+    url = "#{@url}/set/#{key}"
+    internet = Async::HTTP::Internet.new
+
+    begin
+      Async::Task.current.with_timeout(REQUEST_TIMEOUT_IN_SECONDS) do
+        internet.post(url, @headers, [value.to_json])
+      end
+    ensure
+      internet.close
     end
-
-    def save_chat_timezones(chat_id, timezones)
-        set_hash("chat:#{chat_id}:timezones", timezones)
-    end
-
-    private
-
-    def get_hash(key)
-        url = "#{@url}/get/#{key}"
-        internet = Async::HTTP::Internet.new
-
-        begin
-            response = Async::Task.current.with_timeout(REQUEST_TIMEOUT_IN_SECONDS) do
-                internet.get(url, @headers)
-            end
-
-            unless response.success?
-                raise "Upstash API error. Status: #{response.status}. Body: #{response.read}" 
-            end
-
-            body = response.read
-            result = JSON.parse(body)['result']
-
-            return {} if result.nil?
-
-            JSON.parse(result).to_h
-        ensure            
-            internet.close
-        end
-    end
-
-    def set_hash(key, value)
-        url = "#{@url}/set/#{key}"
-        internet = Async::HTTP::Internet.new
-
-        begin
-            Async::Task.current.with_timeout(REQUEST_TIMEOUT_IN_SECONDS) do
-                internet.post(url, @headers, [value.to_json])
-            end
-        ensure
-            internet.close
-        end
-    end
+  end
 end
