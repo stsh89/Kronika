3b480ff09668f08c017085da66e120049c96a7e1
Stanislav Shandyga <stanislavshandyga@gmail.com>
2026-05-11 06:17:10 +0000

    Lint services

--- app/services/notification_service.rb
+++ app/services/notification_service.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 module Kronika
-    class NotificationService
-        def initialize(client)
-            @client = client
-        end
-
-        def send_message(chat, message)
-            @client.send_message(chat.id, message)
-        end
-
-        def send_html_message(chat, html)
-            @client.send_html_message(chat.id, html)
-        end
+  class NotificationService
+    def initialize(client)
+      @client = client
     end
+
+    def send_message(chat, message)
+      @client.send_message(chat.id, message)
+    end
+
+    def send_html_message(chat, html)
+      @client.send_html_message(chat.id, html)
+    end
+  end
 end
--- app/services/storage_service.rb
+++ app/services/storage_service.rb
@@ -1,24 +1,26 @@
+# frozen_string_literal: true
+
 module Kronika
-    class StorageService
-        def initialize(client)
-            @client = client
-        end
-
-        def get_chat(chat_id)
-            timezones = @client.get_chat_timezones(chat_id)
-
-            users = timezones.each_with_object({}) do |(username, tz_identifier), acc|
-                acc[username] = User.new(username: username, timezone: Timezone.new(tz_identifier))
-            end
-
-            Chat.new(
-                id: chat_id,
-                users: users
-            )
-        end
-
-        def save_chat(chat)
-            @client.save_chat_timezones(chat.id, chat.users.transform_values { |user| user.timezone.identifier })
-        end
+  class StorageService
+    def initialize(client)
+      @client = client
     end
+
+    def get_chat(chat_id)
+      timezones = @client.get_chat_timezones(chat_id)
+
+      users = timezones.each_with_object({}) do |(username, tz_identifier), acc|
+        acc[username] = User.new(username: username, timezone: Timezone.new(tz_identifier))
+      end
+
+      Chat.new(
+        id: chat_id,
+        users: users
+      )
+    end
+
+    def save_chat(chat)
+      @client.save_chat_timezones(chat.id, chat.users.transform_values { |user| user.timezone.identifier })
+    end
+  end
 end
