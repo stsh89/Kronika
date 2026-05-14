184c694d88891c232883952464643d74a070152f
Stanislav Shandyga <stanislavshandyga@gmail.com>
2026-05-14 06:32:29 +0000

    Update vagrant configuration

---
+++ .vagrant/config.rb
@@ -0,0 +1,36 @@
+require 'yaml'
+
+Config = Data.define(
+  :git_name,
+  :git_email,
+  :github_user,
+  :github_token,
+  :virtualbox_memory,
+  :virtualbox_cpus,
+  :ruby_version
+)
+
+class Config
+  def git_remote
+    auth = "#{github_user}:#{github_token}"
+
+    "https://#{auth}@github.com/#{github_user}/Kronika.git"
+  end
+
+  class << self
+    def load_from_yaml
+      current_dir = File.dirname(File.expand_path(__FILE__))
+      yaml = YAML.load_file("#{current_dir}/config.yml")
+
+      Config.new(
+        git_name: yaml['git']['name'],
+        git_email: yaml['git']['email'],
+        github_user: yaml['github']['user'],
+        github_token: yaml['github']['token'],
+        virtualbox_memory: yaml['virtualbox']['memory'],
+        virtualbox_cpus: yaml['virtualbox']['cpus'],
+        ruby_version: [4, 0, 4]
+      )
+    end
+  end
+end
---
+++ .vagrant/provision.sh
@@ -0,0 +1,103 @@
+# Setup helix
+
+pacman -Syu --noconfirm --needed helix
+
+mkdir -p /home/vagrant/.config/helix/
+
+cat <<EOF > /home/vagrant/.config/helix/config.toml
+theme = "catppuccin_mocha"
+
+[editor]
+true-color = true
+line-number = "relative"
+cursorline = true
+color-modes = true
+
+[editor.cursor-shape]
+insert = "bar"
+normal = "block"
+select = "underline"
+
+[editor.indent-guides]
+render = true
+
+EOF
+
+chown -R vagrant:vagrant /home/vagrant/.config/helix/
+
+# Setup zellij
+
+pacman -Syu --noconfirm --needed zellij
+
+mkdir -p /home/vagrant/.config/zellij/
+chown -R vagrant:vagrant /home/vagrant/.config/zellij/
+
+# Setup fish
+
+pacman -Syu --noconfirm --needed fish
+
+mkdir -p /home/vagrant/.config/fish
+
+cat <<EOF > /home/vagrant/.config/fish/config.fish
+cd $APP_DIR
+
+if status is-interactive
+    if not set -q ZELLIJ
+        exec zellij options --default-shell fish
+    end
+end
+
+EOF
+
+chown -R vagrant:vagrant /home/vagrant/.config/fish/
+
+# Setup ruby
+
+pacman -Syu --noconfirm --needed base-devel \
+  openssl  zlib libyaml libffi
+
+if ! command -v ruby >/dev/null 2>&1; then
+  BASE_URL="https://cache.ruby-lang.org/pub/ruby"
+  BASE_PATH="$RUBY_VERSION_MAJOR.$RUBY_VERSION_MINOR"
+  DOWNLOAD_URL="$BASE_URL/$BASE_PATH/ruby-$RUBY_VERSION.tar.gz"
+
+  mkdir ~/ruby-build && cd ~/ruby-build
+  curl -O $DOWNLOAD_URL
+  tar -xzf ruby-$RUBY_VERSION.tar.gz
+  cd ruby-$RUBY_VERSION
+  ./configure
+  make -j$(grep -c ^processor /proc/cpuinfo)
+  make install
+  cd ~/ && rm -rf ~/ruby-build
+fi
+
+# Setup docker
+
+pacman -Syu --noconfirm --needed docker
+
+systemctl enable --now docker.service
+usermod -aG docker vagrant
+
+# Setup lazygit
+
+pacman -Syu --noconfirm --needed lazygit
+
+mkdir -p /home/vagrant/.config/lazygit
+chown -R vagrant:vagrant /home/vagrant/.config/lazygit/
+
+# Setup git
+
+pacman -Syu --noconfirm --needed git less
+
+sudo -u vagrant git config --global user.name "$GIT_NAME"
+sudo -u vagrant git config --global user.email "$GIT_EMAIL"
+sudo -u vagrant git config --global core.editor "helix"
+
+if [ ! -d "$APP_DIR" ]; then
+  sudo -u vagrant git clone $GITHUB_REMOTE $APP_DIR
+fi
+
+# Setup app
+
+cd $APP_DIR
+sudo -u vagrant bundle install
