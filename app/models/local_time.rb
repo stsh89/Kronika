12dc9bbaf1e65aa44383722d294642774640cb36
Stanislav Shandyga <stanislavshandyga@gmail.com>
2026-05-18 05:17:03 +0000

    Minor code fix

--- app/models/local_time.rb
+++ app/models/local_time.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
 module Kronika
-  class << self
-    def from_string(time_str, timezone)
-      parsed = Time.strptime(time_str, '%H:%M')
-      tz = TZInfo::Timezone.get(timezone.identifier)
-      now = tz.now
-      time = tz.local_time(now.year, now.month, now.day, parsed.hour, parsed.min).to_time
-
-      new(time, timezone)
-    rescue ArgumentError
-      raise InvalidArgumentError, "Invalid time: `#{time_str}`."
-    end
-  end
-
   class LocalTime
+    class << self
+      def from_string(time_str, timezone)
+        parsed = Time.strptime(time_str, '%H:%M')
+        tz = TZInfo::Timezone.get(timezone.identifier)
+        now = tz.now
+        time = tz.local_time(now.year, now.month, now.day, parsed.hour, parsed.min).to_time
+
+        new(time, timezone)
+      rescue ArgumentError
+        raise InvalidArgumentError, "Invalid time: `#{time_str}`."
+      end
+    end
+
     def initialize(time, timezone)
       @time = time
       @timezone = timezone
