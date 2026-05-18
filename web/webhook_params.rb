037ba9fc6d0623639ca2201c48343bb25dcb30cd
Stanislav Shandyga <stanislavshandyga@gmail.com>
2026-05-18 19:15:01 +0000

    Fix webhook params

--- web/webhook_params.rb
+++ web/webhook_params.rb
@@ -7,17 +7,17 @@ module Web
     class << self
       def from_message!(message)
         new(
-          chat_id: get_value!(payload, %i[chat id]),
-          chat_type: get_value!(payload, %i[chat type]),
-          user_id: get_value!(payload, %i[from id]),
+          chat_id: get_value!(message, %i[chat id]),
+          chat_type: get_value!(message, %i[chat type]),
+          user_id: get_value!(message, %i[from id]),
           message:
         )
       end
 
       private
 
-      def get_value!(payload, path)
-        value = payload.dig(*path)
+      def get_value!(message, path)
+        value = message.dig(*path)
 
         raise "Unexpected webhook message: missing #{path.join('.')} path. Message: #{message}" if value.to_s.empty?
 
