15dc0ff541fcc27e13d7f19350854346265b3bf6
Stanislav Shandyga <stanislavshandyga@gmail.com>
2026-05-19 08:16:17 +0000

    Fix time normalization

--- clients/global_time.rb
+++ clients/global_time.rb
@@ -4,8 +4,22 @@ require 'tzinfo'
 
 module GlobalTime
   class Timezone
+    def initialize
+      @client = TZInfo::Timezone
+    end
+
     def time_now(identifier)
-      TZInfo::Timezone.get(identifier)
+      tz = timezone(identifier)
+
+      return unless tz
+
+      tz.now
+    end
+
+    private
+
+    def timezone(identifier)
+      @client.get(identifier)
     rescue TZInfo::InvalidTimezoneIdentifier
       nil
     end
