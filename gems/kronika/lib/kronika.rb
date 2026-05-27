b792b8f6e58d5c71f748a3d9ce3e2960acd9ae00
Stanislav Shandyga <stanislavshandyga@gmail.com>
2026-05-27 08:04:36 +0000

    Minor code update

--- gems/kronika/lib/kronika.rb
+++ gems/kronika/lib/kronika.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
 require_relative 'kronika/access_key'
-require_relative 'kronika/chrono'
 require_relative 'kronika/location'
-require_relative 'kronika/repository'
 require_relative 'kronika/tenant'
 require_relative 'kronika/timestamp'
 require_relative 'kronika/timezone'
 
 module Kronika
+  autoload :Chrono, 'kronika/chrono'
+  autoload :Repository, 'kronika/repository'
+
   autoload :ConvertTimeOperation, 'kronika/convert_time_operation'
   autoload :ReadTimezoneOperation, 'kronika/read_timezone_operation'
-  autoload :DropTimezoneOperation, 'kronika/drop_timezone_operation.rb'
+  autoload :DropTimezoneOperation, 'kronika/drop_timezone_operation'
   autoload :SaveTimezoneOperation, 'kronika/save_timezone_operation'
 end
